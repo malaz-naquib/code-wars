efe29d3537f2ad15af98a045119ca35cd852c39f
Malaz Naquib <malaz.naquib1@gmail.com>
2023-04-25 14:37:49 -0700

    Completed daily kata solution

--- 6 Kyu/playing-with-digits.js
+++ 6 Kyu/playing-with-digits.js
@@ -22,3 +22,27 @@
 // digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 // digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 
+function digPow(n, p) {
+  let string = n.toString();
+  let len = string.length;
+  let result = 0;
+  for(let i = 0; i < len ; i++) {
+    let numberser = parseInt(string.charAt(i), 10);
+    result +=  Math.pow(numberser, p + i);
+  }
+  let x = Math.pow(n,p);
+  if(result === x){
+    return p;
+    }else if(result % n === 0) {
+    return result / n;
+  }else {
+    return -1;
+  }
+}
+
+// OR:
+
+function digPow(n, p) {
+  let x = String(n).split('').reduce((s, d, i) => s + Math.pow(d, p + i), 0)
+  return x % n ? -1 : x / n
+}
