4506753d68254da894be5005e40cb6da2deabf84
Malaz Naquib <malaz.naquib1@gmail.com>
2023-05-17 11:05:25 -0700

    Completed daily 7 kyu kata

---
+++ 7 Kyu/shortest-word.js
@@ -0,0 +1,15 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    let words = s.split(" ");
+    let shortest = words[0].length;
+    let next;
+    
+    for(let i = 1; i < words.length; i++){
+      next = words[i].length;
+      if(shortest > next) shortest = next;
+    }
+    return shortest;
+};
