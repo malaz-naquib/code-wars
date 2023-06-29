2f542d42ed75b425a2601c680e5355c3d4083520
Malaz Naquib <malaz.naquib1@gmail.com>
2023-06-29 14:01:16 -0700

    Completed daily kata solution

---
+++ 7 Kyu/count-consonants.js
@@ -0,0 +1,5 @@
+// Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+// Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+const consonantCount = str => str.replace(/[^a-z]|[aeiou]/gi, '').length;
