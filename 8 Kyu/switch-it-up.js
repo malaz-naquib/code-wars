da9bed48fe56c5a9ace7b1833e8d0b6f1a938a44
Malaz Naquib <malaz.naquib1@gmail.com>
2023-07-06 11:31:38 -0700

    Completed daily kata solution

---
+++ 8 Kyu/switch-it-up.js
@@ -0,0 +1,9 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+switchItUp = number => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number];
