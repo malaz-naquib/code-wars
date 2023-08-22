261ef40f774e15c293c1d3a512345c95bdf3b0bb
Malaz Naquib <malaz.naquib1@gmail.com>
2023-08-22 12:28:17 -0700

    Completed daily kata solution

---
+++ 7 Kyu/find-the-capitals.js
@@ -0,0 +1,15 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+var capitals = function(word) {
+  let capArr = [];
+  for(let i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() === word[i]) {
+      capArr.push(i);
+    }
+  }
+  return capArr;
+}
