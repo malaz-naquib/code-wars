257791ad9743534f83f3e853862c69bd2e2589c5
Malaz Naquib <malaz.naquib1@gmail.com>
2023-09-17 12:29:06 -0700

    Completed daily kata solution

---
+++ 6 Kyu/build-tower.js
@@ -0,0 +1,40 @@
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+// Go challenge Build Tower Advanced once you have finished this :)
+
+function towerBuilder(nFloors) {
+  let space, star, tower = [];
+  for(i = 1; i <= nFloors; i++) {
+    space = " ".repeat(nFloors - i);
+    star  = "*".repeat((2 * i) - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+
+// OR
+
+function towerBuilder(n) {
+  return Array.from({length: n}, function(v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
